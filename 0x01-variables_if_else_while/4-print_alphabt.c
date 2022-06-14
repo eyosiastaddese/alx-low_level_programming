6d2ad99b7b0c627470de7a982506014dff1d15b9
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-06-14 14:32:13 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,12 +12,12 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch == 'q' || ch == 'e')
+		if (ch == 'e' || ch == 'q')
 			++ch;
 		putchar(ch);
 		++ch;
 	}
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
