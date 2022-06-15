dfd5fd7e49515e32ac973ca8557313b5c9f713ba
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-06-15 13:34:47 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - printing base16
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		++a;
+	}
+	char ch = 'a';
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
