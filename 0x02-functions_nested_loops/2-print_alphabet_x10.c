a435053ef1277252fc0435aef21f2b20e04b1584
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-06-20 13:40:30 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints an alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9 ++line)
+	{
+		for (ch = 'a'; ch <= 'z' ++ch)
+			_pitchar(ch);
+		_putchar('\n');
+	}
+}
