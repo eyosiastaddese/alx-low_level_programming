47cc8f4dc99457f56fd125797456aee7cbab308a
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-06-21 06:00:14 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prins the last digit
+ *
+ * @n: to be computed
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n);
+{
+	int ch;
+
+	ch = n % 10;
+	if (ch < 0)
+	{
+		ch *= -1;
+	}
+	_putchar('0' + ch);
+	return (ch);
+}
