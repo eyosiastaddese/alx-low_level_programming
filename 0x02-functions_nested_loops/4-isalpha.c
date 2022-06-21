0000c432416a82229db3d57fa09b9ffde85ef9a0
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-06-21 05:14:09 -0700

    _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabet
+ *
+ * @c: takes input from another function
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
