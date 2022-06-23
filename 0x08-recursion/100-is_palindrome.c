ab1b9744c88966cb22f0b088d59cf1d9e83a1ec2
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-06-23 03:39:28 -0700

    is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		s++;
+		return (1  _strlen_recursion(s));
+	}
+	return (0);
+}
+
+int checker(char, *str int len, int count)
+{
+	if (count >= len)
+		return (1);
+	if (str[len] == str[count])
+		return (checker(str, len - 1, count + 1));
+	return (0);
+}
+
+int is_palindrome(char *s)
+{
+	int len = _strlrn_recursion(s);
+	int count = 0;
+
+	return (checker(s, len - 1, count));
+}
