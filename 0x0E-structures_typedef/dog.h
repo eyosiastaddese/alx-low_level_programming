ea0317a3be4d3117cc99b19342e7566256735bff
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-07-11 13:14:36 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,15 +8,22 @@
  * @owner: owner of dog
  *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+#endif
+
+#ifndef MAIN_H
+#define MAIN_H
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 typedef struct dog dog_t;
 
