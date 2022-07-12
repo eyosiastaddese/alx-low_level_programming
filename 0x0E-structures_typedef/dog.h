204cb543141b9b9c392d52768648a77dbdb4613c
eyosiastaddese <eyosiastaddse12@gmail.com>
2022-07-12 14:36:52 -0700

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-#endif
-
-#ifndef MAIN_H
-#define MAIN_H
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
